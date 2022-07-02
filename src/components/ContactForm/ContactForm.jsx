c6d32ae1fe6c91690ebc4d134bf795f7ff46b4c3
Rostyslav-Baryshok <85187900+Rostyslav-Baryshok@users.noreply.github.com>
2022-07-02 15:38:16 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Formik, Form, Field } from 'formik';
 
 class ContactForm extends Component {
   state = {
@@ -20,35 +21,37 @@ class ContactForm extends Component {
   render() {
     const { name, number } = this.state;
     return (
-      <form action="submit" onSubmit={this.handleSubmit}>
-        <div>
-          <label>
-            <span>Name</span>
-            <input
-              type="text"
-              name="name"
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required
-              value={name}
-              onChange={this.handleInputChange}
-            />
-          </label>
-          <label>
-            <span>Number</span>
-            <input
-              type="tel"
-              name="number"
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-              required
-              value={number}
-              onChange={this.handleInputChange}
-            />
-          </label>
-          <button type="submit">Add contact</button>
-        </div>
-      </form>
+      <Formik>
+        <Form action="submit" onSubmit={this.handleSubmit}>
+          <div>
+            <label>
+              <span>Name</span>
+              <Field
+                type="text"
+                name="name"
+                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                required
+                value={name}
+                onChange={this.handleInputChange}
+              />
+            </label>
+            <label>
+              <span>Number</span>
+              <Field
+                type="tel"
+                name="number"
+                pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                required
+                value={number}
+                onChange={this.handleInputChange}
+              />
+            </label>
+            <button type="submit">Add contact</button>
+          </div>
+        </Form>
+      </Formik>
     );
   }
 }
